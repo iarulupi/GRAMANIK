24cdea5e003fbc0370828adc744b5edadbd2aaf6
iarulupi <iaru.lupi@gmail.com>
2026-08-24 20:26:49 -0300

    Add files via upload

---
+++ GRAMANICK/js/simbolos.js
@@ -0,0 +1,21 @@
+// ==================== DATOS ====================
+
+const simboloAdjetivo = "https://i.ibb.co/gFLMWsdC/Whats-App-Image-2026-02-26-at-5-33-08-PM.jpg";
+const simboloPronombre = "https://i.ibb.co/jZL13LrK/simbolo-pronombre.png";
+const simboloRelacionante = "https://i.ibb.co/d4tdpfxJ/simbolo-relacionante.png";
+const simboloVerboInfinitivo = "https://i.ibb.co/dwB90kGz/Whats-App-Image-2026-02-26-at-5-00-49-PM.jpg";
+const simboloReflexivosCuasiReflejos = "https://i.ibb.co/vC9pXdML/Whats-App-Image-2026-03-07-at-14-06-26.jpg";
+
+// palabras que pueden escribirse diferente y las reconoce igual
+const aliasNormalizacion = {
+    "cafecito": "café",
+    "tele": "television",
+    "verbos": "verbo",
+    "dispositivos móviles": "dispositivos electrónicos",
+    "oración": "oraciones",
+};
+
+
+// ==================== DICCIONARIO GENERAL ====================
+
+let pictogramas = {};
---
+++ GRAMANICK/js/vocabulario/transportes.js
@@ -0,0 +1,23 @@
+// ==================== TRANSPORTES ====================
+
+const vocabularioTransportes = {
+    "auto": "https://static.arasaac.org/pictograms/2339/2339_300.png",
+    "avión": "https://static.arasaac.org/pictograms/2264/2264_300.png",
+
+    "moto": [
+        "https://static.arasaac.org/pictograms/7166/7166_300.png",
+        "https://static.arasaac.org/pictograms/38117/38117_300.png"
+    ],
+
+    "colectivo": [
+        "https://static.arasaac.org/pictograms/2263/2263_300.png",
+        "https://static.arasaac.org/pictograms/36962/36962_300.png"
+    ],
+
+    "bicicleta": [
+        "https://static.arasaac.org/pictograms/2277/2277_300.png",
+        "https://static.arasaac.org/pictograms/6045/6045_300.png"
+    ],
+};
+
+Object.assign(pictogramas, vocabularioTransportes);
