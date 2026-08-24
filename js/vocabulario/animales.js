82106dc7006745821d80c6bcfdcf4674b78b71b3
iarulupi <iaru.lupi@gmail.com>
2026-08-24 20:53:45 -0300

    Update animales.js

--- js/vocabulario/animales.js
+++ js/vocabulario/animales.js
@@ -1,10 +1,54 @@
 // ==================== ANIMALES ====================
 
 const vocabularioAnimales = {
+    "animales": "https://static.arasaac.org/pictograms/6901/6901_300.png",
+    "animal": "https://static.arasaac.org/pictograms/8025/8025_300.png",
+    "pez": "https://static.arasaac.org/pictograms/2520/2520_300.png",
+    "murciélago": "https://static.arasaac.org/pictograms/2903/2903_300.png",
+    "cocodrilo": "https://static.arasaac.org/pictograms/2343/2343_300.png",
+    "ornitorrinco": "https://static.arasaac.org/pictograms/13090/13090_300.png",
+    "perezoso": "https://static.arasaac.org/pictograms/22039/22039_300.png",
+    "cisne": "https://static.arasaac.org/pictograms/2337/2337_300.png",
+    "elefante": "https://static.arasaac.org/pictograms/2372/2372_300.png",
+    "hipopótamo": "https://static.arasaac.org/pictograms/2424/2424_300.png",
+   "gato": [
+    "https://static.arasaac.org/pictograms/2406/2406_300.png",
+    "https://static.arasaac.org/pictograms/7114/7114_300.png",
+    "https://static.arasaac.org/pictograms/9881/9881_300.png",
+    "https://static.arasaac.org/pictograms/9879/9879_300.png",
+    "https://static.arasaac.org/pictograms/38968/38968_300.png"
+],
+"perro": [
+    "https://static.arasaac.org/pictograms/2517/2517_300.png",
+    "https://static.arasaac.org/pictograms/7202/7202_300.png",
+    "https://static.arasaac.org/pictograms/38967/38967_300.png",
+    "https://static.arasaac.org/pictograms/26302/26302_300.png",
+    "https://static.arasaac.org/pictograms/35997/35997_300.png"
+    "https://static.arasaac.org/pictograms/9016/9016_300.png",
+    "https://static.arasaac.org/pictograms/36424/36424_300.png",
+    "https://static.arasaac.org/pictograms/29738/29738_300.png",
+     "https://static.arasaac.org/pictograms/10145/10145_300.png"    
+], 
+    "gatito": "https://static.arasaac.org/pictograms/34082/34082_300.png",
     "caballo": [
         "https://static.arasaac.org/pictograms/2294/2294_300.png",
         "https://static.arasaac.org/pictograms/27627/27627_300.png"
     ],
+    "serpiente": "https://static.arasaac.org/pictograms/2568/2568_300.png",
+    "anaconda": "https://static.arasaac.org/pictograms/38634/38634_300.png",
+    "loro": "https://static.arasaac.org/pictograms/2562/2562_300.png",
+    "oso": "https://static.arasaac.org/pictograms/2868/2868_300.png",
+    "canario": "https://static.arasaac.org/pictograms/3004/3004_300.png",
+    "pinguino": "https://static.arasaac.org/pictograms/3243/3243_300.png",
+    "poni": "https://static.arasaac.org/pictograms/8324/8324_300.png",
+    "hámster": "https://static.arasaac.org/pictograms/3346/3346_300.png",
+    "pájaro": "https://static.arasaac.org/pictograms/26271/26271_300.png",
+    "tortuga": "https://static.arasaac.org/pictograms/26503/26503_300.png",
+    "lobo marino": "https://static.arasaac.org/pictograms/27501/27501_300.png",
+    "pato": "https://static.arasaac.org/pictograms/28477/28477_300.png",
+    "camello": "https://static.arasaac.org/pictograms/2305/2305_300.png",
+    "canguro", "https://static.arasaac.org/pictograms/2313/2313_300.png",
+    "cebra": "https://static.arasaac.org/pictograms/2324/2324_300.png",
 };
 
 Object.assign(pictogramas, vocabularioAnimales);
