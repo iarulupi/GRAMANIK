cf312e325dde5376dd60c69f202527d9e1307c55
iarulupi <iaru.lupi@gmail.com>
2026-08-24 20:27:55 -0300

    Delete GRAMANICK directory

--- GRAMANICK/js/vocabulario/deportes.js
+++
@@ -1,8 +0,0 @@
-// ==================== DEPORTES ====================
-
-const vocabularioDeportes = {
-    "fútbol": "https://i.ibb.co/pv5XYzkK/futbol.jpg",
-    "ténis": "https://i.ibb.co/YBCJW4PF/tenis.jpg",
-};
-
-Object.assign(pictogramas, vocabularioDeportes);
