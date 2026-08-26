0cefb41fd3a98e3a3ab16207761b991e4218184a
iarulupi <iaru.lupi@gmail.com>
2026-08-25 21:13:51 -0300

    Add files via upload

---
+++ js/vocabulario/preposiciones.js
@@ -0,0 +1,26 @@
+// ==================== PREPOSICIONES ====================
+
+let preposiciones = [
+    "a",
+    "del",
+    "al",
+    "ante",
+    "bajo",
+    "cabe",
+    "con",
+    "contra",
+    "de",
+    "desde",
+    "durante",
+    "en",
+    "entre",
+    "hacia",
+    "hasta",
+    "mediante",
+    "para",
+    "por",
+    "segun",
+    "sin",
+    "sobre",
+    "tras"
+];
