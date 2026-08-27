829261125dfe991bb539f9ff18fdb8e22a5e5aba
iarulupi <iaru.lupi@gmail.com>
2026-08-27 15:15:41 -0300

    Update clima.js

--- js/vocabulario/clima.js
+++ js/vocabulario/clima.js
@@ -1,9 +1,78 @@
 // ==================== CLIMA ====================
 
 const vocabularioClima = {
-"lluvia": "https://static.arasaac.org/pictograms/7148/7148_300.png",
-    "sol": "https://static.arasaac.org/pictograms/7252/7252_300.png",
-    "nubes": "https://static.arasaac.org/pictograms/34383/34383_300.png",
+
+    "clima": "URL CLIMA",
+
+    "sol": "URL SOL",
+
+    "nube": "URL NUBE",
+    "nubes": "URL NUBES",
+
+    "lluvia": "URL LLUVIA",
+    "llovizna": "URL LLOVIZNA",
+
+    "tormenta": "URL TORMENTA",
+    "tormenta eléctrica": "URL TORMENTA ELÉCTRICA",
+
+    "rayo": "URL RAYO",
+    "rayos": "URL RAYOS",
+
+    "relámpago": "URL RELÁMPAGO",
+    "relámpagos": "URL RELÁMPAGOS",
+
+    "trueno": "URL TRUENO",
+    "truenos": "URL TRUENOS",
+
+    "viento": "URL VIENTO",
+
+    "niebla": "URL NIEBLA",
+
+    "granizo": "URL GRANIZO",
+
+    "nieve": "URL NIEVE",
+
+    "arcoíris": "URL ARCOÍRIS",
+
+
+    // TEMPERATURA
+
+    "calor": "URL CALOR",
+    "frío": "URL FRÍO",
+
+    "temperatura": "URL TEMPERATURA",
+    "termómetro": "URL TERMÓMETRO",
+
+
+    // FENÓMENOS NATURALES
+
+    "inundación": "URL INUNDACIÓN",
+
+    "sequía": "URL SEQUÍA",
+
+    "tornado": "URL TORNADO",
+
+    "huracán": "URL HURACÁN",
+
+    "ciclón": "URL CICLÓN",
+
+    "terremoto": "URL TERREMOTO",
+
+    "temblor": "URL TEMBLOR",
+
+    "tsunami": "URL TSUNAMI",
+
+    "volcán": "URL VOLCÁN",
+
+    "erupción": "URL ERUPCIÓN",
+
+    "erupción volcánica": "URL ERUPCIÓN VOLCÁNICA",
+
+    "avalancha": "URL AVALANCHA",
+
+    "alud": "URL ALUD",
+
+    "incendio forestal": "URL INCENDIO FORESTAL"
 };
 
 Object.assign(pictogramas, vocabularioClima);
