3f5428ea0bdc430c0b89ca55dd25fbb9865edc2e
iarulupi <iaru.lupi@gmail.com>
2026-08-28 00:18:03 -0300

    Add files via upload

---
+++ js/vocabulario/deportes.js
@@ -0,0 +1,8 @@
+// ==================== DEPORTES ====================
+
+const vocabularioDeportes = {
+    "fútbol": "https://i.ibb.co/pv5XYzkK/futbol.jpg",
+    "ténis": "https://i.ibb.co/YBCJW4PF/tenis.jpg",
+};
+
+Object.assign(pictogramas, vocabularioDeportes);
---
+++ js/vocabulario/escuela.js
@@ -0,0 +1,41 @@
+// ==================== ESCUELA ====================
+
+const vocabularioEscuela = {
+"lengua": "https://static.arasaac.org/pictograms/36371/36371_300.png",
+    "matemática": "https://static.arasaac.org/pictograms/32554/32554_300.png",
+    "ciencias": "https://static.arasaac.org/pictograms/35437/35437_300.png",
+    "arte": "https://static.arasaac.org/pictograms/13634/13634_300.png",
+    "música": "https://static.arasaac.org/pictograms/24795/24795_300.png",
+    "educación física": "https://static.arasaac.org/pictograms/10258/10258_300.png",
+    "piscomotricidad": "https://static.arasaac.org/pictograms/32552/32552_300.png",
+
+
+ "empezaron las clases": "https://i.ibb.co/TqNbxMjH/empezaron-las-clases.jpg",
+
+"clase": "https://static.arasaac.org/pictograms/39686/39686_300.png",
+
+    "conjunto": "https://i.ibb.co/kv3MFFZ/conjunto.jpg",
+    "números": "https://static.arasaac.org/pictograms/2879/2879_300.png",
+    "figuras geométricas": "https://i.ibb.co/7Jw75Pgh/Whats-App-Image-2026-03-01-at-8-31-29-PM.jpg",
+    "triángulo": "https://static.arasaac.org/pictograms/4763/4763_300.png",
+    "cuadrado": "https://static.arasaac.org/pictograms/4616/4616_300.png",
+    "rectángulo": "https://static.arasaac.org/pictograms/4731/4731_300.png",
+    "círculo ": "https://static.arasaac.org/pictograms/4603/4603_300.png",
+
+
+    "noticia": "",
+    "diálogo": "https://static.arasaac.org/pictograms/23402/23402_300.png",
+    "composición": "https://i.ibb.co/XrF4G9mD/composion.jpg",
+    "oración": "https://i.ibb.co/Cp0276jq/oracion.jpg",
+    "relato": "https://i.ibb.co/MDB0HdKK/relato.jpg",
+    "palabra": "https://i.ibb.co/fVkvCxqB/palabra.jpg",
+    "párrafo": "https://i.ibb.co/FbZ1SH5C/parrafo.jpg",
+
+
+"verbo": "https://static.arasaac.org/pictograms/32604/32604_300.png",
+
+    "jesús": "https://static.arasaac.org/pictograms/24135/24135_300.png",
+    "la virgen maría": "https://static.arasaac.org/pictograms/7301/7301_300.png",
+};
+
+Object.assign(pictogramas, vocabularioEscuela);
