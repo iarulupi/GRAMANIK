67a52352a5b2564b0b590a305a63531d09d39fad
iarulupi <iaru.lupi@gmail.com>
2026-08-28 18:55:04 -0300

    Update electronica.js

--- js/vocabulario/electronica.js
+++ js/vocabulario/electronica.js
@@ -1,14 +1,172 @@
-// ==================== ELECTRONICA ====================
+// ==================== ELECTRÓNICA ====================
 
 const vocabularioElectronica = {
-"dispositivos electrónicos": "https://static.arasaac.org/pictograms/34906/34906_300.png",
-    "televisor": "https://static.arasaac.org/pictograms/25498/25498_300.png",
-    "tablet": "https://static.arasaac.org/pictograms/29151/29151_300.png",
+
+    // ==================== GENERAL ====================
+
+    "dispositivo electrónico": "https://static.arasaac.org/pictograms/34906/34906_300.png",
+    "dispositivos electrónicos": "https://static.arasaac.org/pictograms/34906/34906_300.png",
+
+    "tecnología": "",
+    "electrónica": "",
+
+    // ==================== COMPUTADORAS ====================
+
     "computadora": "https://static.arasaac.org/pictograms/7190/7190_300.png",
+    "computadoras": "https://static.arasaac.org/pictograms/7190/7190_300.png",
+
     "notebook": "https://static.arasaac.org/pictograms/7214/7214_300.png",
-    "luz": "https://static.arasaac.org/pictograms/8619/8619_300.png",
-    "ventilador": "https://static.arasaac.org/pictograms/2612/2612_300.png",
+    "notebooks": "https://static.arasaac.org/pictograms/7214/7214_300.png",
+
+    "monitor": "",
+    "monitores": "",
+
+    "pantalla": "",
+    "pantallas": "",
+
+    "teclado": "",
+    "teclados": "",
+
+    "mouse": "",
+    "ratón": "",
+
+    "impresora": "",
+    "impresoras": "",
+
+    "parlante": "",
+    "parlantes": "",
+
+    "auricular": "",
+    "auriculares": "",
+
+    "micrófono": "",
+    "micrófonos": "",
+
+    "cámara web": "",
+    "webcam": "",
+
+    // ==================== CELULARES Y TABLETS ====================
+
     "celular": "https://static.arasaac.org/pictograms/25269/25269_300.png",
+    "celulares": "https://static.arasaac.org/pictograms/25269/25269_300.png",
+
+    "teléfono": "",
+    "teléfonos": "",
+
+    "teléfono celular": "https://static.arasaac.org/pictograms/25269/25269_300.png",
+
+    "tablet": "https://static.arasaac.org/pictograms/29151/29151_300.png",
+    "tablets": "https://static.arasaac.org/pictograms/29151/29151_300.png",
+
+    // ==================== TELEVISIÓN ====================
+
+    "televisor": "https://static.arasaac.org/pictograms/25498/25498_300.png",
+    "televisores": "https://static.arasaac.org/pictograms/25498/25498_300.png",
+
+    "televisión": "https://static.arasaac.org/pictograms/25498/25498_300.png",
+    "tele": "https://static.arasaac.org/pictograms/25498/25498_300.png",
+
+    "control remoto": "",
+    "controles remotos": "",
+
+    // ==================== AUDIO ====================
+
+    "radio": "",
+
+    "equipo de música": "",
+
+    "grabador": "",
+
+    // ==================== VIDEO Y FOTOGRAFÍA ====================
+
+    "cámara": "",
+    "cámaras": "",
+
+    "cámara de fotos": "",
+    "cámara de video": "",
+
+    // ==================== VIDEOJUEGOS ====================
+
+    "videojuego": "",
+    "videojuegos": "",
+
+    "consola": "",
+    "consolas": "",
+
+    "joystick": "",
+    "joysticks": "",
+
+    "control": "",
+    "controles": "",
+
+    // ==================== INTERNET ====================
+
+    "internet": "",
+    "wifi": "",
+    "wi-fi": "",
+
+    "router": "",
+
+    // ==================== ACCESORIOS ====================
+
+    "cargador": "",
+    "cargadores": "",
+
+    "cable": "",
+    "cables": "",
+
+    "enchufe": "",
+    "enchufes": "",
+
+    "adaptador": "",
+    "adaptadores": "",
+
+    "pendrive": "",
+    "pendrives": "",
+
+    "memoria": "",
+    "tarjeta de memoria": "",
+
+    "disco rígido": "",
+    "disco externo": "",
+
+    "batería": "",
+    "baterías": "",
+
+    "pila": "",
+    "pilas": "",
+
+    // ==================== HOGAR ====================
+
+    "luz": "https://static.arasaac.org/pictograms/8619/8619_300.png",
+    "luces": "https://static.arasaac.org/pictograms/8619/8619_300.png",
+
+    "ventilador": "https://static.arasaac.org/pictograms/2612/2612_300.png",
+    "ventiladores": "https://static.arasaac.org/pictograms/2612/2612_300.png",
+
+    "aire acondicionado": "",
+
+    "microondas": "",
+
+    "heladera": "",
+
+    "freezer": "",
+
+    "lavarropas": "",
+
+    "secador de pelo": "",
+
+    // ==================== PARTES Y ELEMENTOS ====================
+
+    "botón": "",
+    "botones": "",
+
+    "tecla": "",
+    "teclas": "",
+
+    "batería": "",
+    "baterías": ""
+
 };
 
 Object.assign(pictogramas, vocabularioElectronica);
