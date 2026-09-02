3d64bbf82c478ed447557dbd72c1f2ddcbedd029
iarulupi <iaru.lupi@gmail.com>
2026-09-02 10:15:26 -0300

    Update otros.js

--- js/vocabulario/otros.js
+++ js/vocabulario/otros.js
@@ -1,49 +1,100 @@
 // ==================== OTROS ====================
 
 const vocabularioOtros = {
-"cortar el pelo": "https://static.arasaac.org/pictograms/27696/27696_300.png",
+
+    "cortar el pelo": "https://static.arasaac.org/pictograms/27696/27696_300.png",
     "pelo": "https://static.arasaac.org/pictograms/2851/2851_300.png",
 
     "foto": "https://static.arasaac.org/pictograms/7107/7107_300.png",
     "imagen": "https://static.arasaac.org/pictograms/7107/7107_300.png",
-  "imagenes": "https://static.arasaac.org/pictograms/7107/7107_300.png",
-   
+    "imagenes": "https://static.arasaac.org/pictograms/7107/7107_300.png",
 
-  "piñata": "https://static.arasaac.org/pictograms/31406/31406_300.png",
-  
+    "piñata": "https://static.arasaac.org/pictograms/31406/31406_300.png",
+
     "pintarse las uñas": "https://static.arasaac.org/pictograms/5540/5540_300.png",
     "pintó las uñas": "https://static.arasaac.org/pictograms/5540/5540_300.png",
     "esmalte": "https://static.arasaac.org/pictograms/2747/2747_300.png",
 
- "semillas": "https://static.arasaac.org/pictograms/39192/39192_300.png",
+    "semillas": "https://static.arasaac.org/pictograms/39192/39192_300.png",
     "plantas": "https://static.arasaac.org/pictograms/11572/11572_300.png",
     "planta": "https://static.arasaac.org/pictograms/3143/3143_300.png",
     "semilla": "https://static.arasaac.org/pictograms/8689/8689_300.png",
-"flores": "https://static.arasaac.org/pictograms/7105/7105_300.png",
-    "flor":"https://static.arasaac.org/pictograms/7104/7104_300.png",
+    "flores": "https://static.arasaac.org/pictograms/7105/7105_300.png",
+    "flor": "https://static.arasaac.org/pictograms/7104/7104_300.png",
     "maceta": "https://static.arasaac.org/pictograms/3126/3126_300.png",
-    
 
- "luna": "https://static.arasaac.org/pictograms/10294/10294_300.png",
+    "luna": "https://static.arasaac.org/pictograms/10294/10294_300.png",
+
     "Tierra": [
         "https://static.arasaac.org/pictograms/30014/30014_300.png",
-        "https://static.arasaac.org/pictograms/3160/3160_300.png",
-        ],
+        "https://static.arasaac.org/pictograms/3160/3160_300.png"
+    ],
+
     "nave espacial": "https://static.arasaac.org/pictograms/2344/2344_300.png",
 
-   
     "dolor": "https://static.arasaac.org/pictograms/30620/30620_300.png",
 
     "ver tele": "https://static.arasaac.org/pictograms/29123/29123_300.png",
     "ver una película": "https://static.arasaac.org/pictograms/34319/34319_300.png",
 
-  
     "carpa": "https://static.arasaac.org/pictograms/4756/4756_300.png",
-    
+
     "silla": "https://static.arasaac.org/pictograms/3155/3155_300.png",
-    "mesa":"https://static.arasaac.org/pictograms/3129/3129_300.png",
-    
-    "dedo":"https://static.arasaac.org/pictograms/3298/3298_300.png",
+    "mesa": "https://static.arasaac.org/pictograms/3129/3129_300.png",
+
+    "dedo": "https://static.arasaac.org/pictograms/3298/3298_300.png",
+
+
+    // ==================== PARTES DE LA CASA ====================
+
+    "living": "https://static.arasaac.org/pictograms/33074/33074_300.png",
+
+    "habitación": [
+        "https://static.arasaac.org/pictograms/5988/5988_300.png",
+        "https://static.arasaac.org/pictograms/33068/33068_300.png"
+    ],
+
+    "dormitorio": [
+        "https://static.arasaac.org/pictograms/5988/5988_300.png",
+        "https://static.arasaac.org/pictograms/33068/33068_300.png"
+    ],
+
+    "terraza": "https://static.arasaac.org/pictograms/22129/22129_300.png",
+
+    "cocina": "https://static.arasaac.org/pictograms/33070/33070_300.png",
+
+    "baño": [
+        "https://static.arasaac.org/pictograms/33954/33954_300.png",
+        "https://static.arasaac.org/pictograms/15905/15905_300.png"
+    ],
+
+    "sótano": "https://static.arasaac.org/pictograms/8223/8223_300.png",
+
+    "jardín": "https://static.arasaac.org/pictograms/37986/37986_300.png",
+
+
+    // ==================== UTENSILIOS DE COCINA ====================
+
+    "plato": [
+        "https://static.arasaac.org/pictograms/2532/2532_300.png",
+        "https://static.arasaac.org/pictograms/16857/16857_300.png"
+    ],
+
+    "taza": "https://static.arasaac.org/pictograms/2582/2582_nocolor_500.png",
+
+    "botella": "https://static.arasaac.org/pictograms/2288/2288_300.png",
+
+    "vaso": "https://static.arasaac.org/pictograms/2610/2610_300.png",
+
+    "cuchillo": "https://static.arasaac.org/pictograms/3237/3237_300.png",
+
+    "tenedor": "https://static.arasaac.org/pictograms/2588/2588_300.png",
+
+    "cuchara": [
+        "https://static.arasaac.org/pictograms/2362/2362_300.png",
+        "https://static.arasaac.org/pictograms/9086/9086_300.png"
+    ]
+
 };
 
 Object.assign(pictogramas, vocabularioOtros);
